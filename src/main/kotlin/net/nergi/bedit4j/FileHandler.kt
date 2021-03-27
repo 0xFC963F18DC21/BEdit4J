0a948b9ac27842008c1578d0e62585e2067068f9
0xFC963F18DC21 <mfabdullah2002@gmail.com>
2021-03-27 19:33:03 +0000

    added file save?

--- src/main/kotlin/net/nergi/bedit4j/FileHandler.kt
+++ src/main/kotlin/net/nergi/bedit4j/FileHandler.kt
@@ -1,11 +1,25 @@
 package net.nergi.bedit4j
 
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.FileReader
+import java.io.FileWriter
 import java.io.Reader
+import java.util.stream.Stream
 
 object FileHandler {
     @JvmStatic
     fun loadFileAsReader(path: String): Reader =
         BufferedReader(FileReader(path))
+
+    @JvmStatic
+    fun saveFileFromStream(stream: Stream<String>, path: String) {
+        val writer = BufferedWriter(FileWriter(path))
+        writer.use { thiz ->
+            stream.forEach {
+                thiz.write(it)
+                thiz.newLine()
+            }
+        }
+    }
 }
